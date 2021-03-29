5abfc6e0a7feef62e56f9cae466df30ed425b205
Mahmoud Al-Dinah <mahmood.dinah2@gmail.com>
2021-03-29 14:49:42 +0300

    added css

---
+++ app1.js
@@ -0,0 +1,46 @@
+// var userAge = prompt('please enteer your age')
+
+// if (userAge >= 18 && userAge < 35 ){
+// alert('Welcome to my website')
+// } else if (userAge < 18 || userAge > 35 ){
+// alert('you are not allowed')
+// }else{
+//     alert('have a nice day')
+// }
+
+
+// var userPass = prompt('enter password');
+// var pass = 'hi';
+
+// if (userPass == pass){
+// alert('welcome يا وحش')
+// }else{
+
+//     userPass = prompt('تاني يا وحش')
+// }
+
+
+// while(userPass !== pass){
+//     userPass = prompt('enter pass again')
+// }
+
+
+// var image = prompt('whats your fav movie' , 'inter , incep')
+// var imageNum = prompt('how many image you would like to see!')
+// var choose = "";
+
+// while (image !== 'inter' && image !== 'incep') {
+//     image = prompt('please choose on of them')
+// }
+
+// for(var i = 1 ; i <=imageNum; i++ ){
+// if (image === 'inter') {
+//     choose = choose + ('img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr3avP9gpf3XDKfaKkcSvlOMDvSHHIVPsyiw&usqp=CAU"/>')
+   
+// }else if (image === 'incep') {
+//     choose = choose + ('img src="https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg"/>')
+    
+// }
+// }
+// console.log(choose);
+// document.write(choose)
